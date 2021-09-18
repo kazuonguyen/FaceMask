d7854ca08eacb28ed1fa30885af8d16b628363ef
nguyenquochung2357 <carzygaming2222@gmail.com>
2021-09-18 17:34:57 +0700

    add gui

--- test_cam.py
+++ test_cam.py
@@ -1,3 +1,9 @@
+# Import required Libraries
+import tkinter as tk
+
+from tkinter import *
+from PIL import Image, ImageTk
+import cv2
 
 import tensorflow.keras
 from PIL import Image, ImageOps
@@ -13,12 +19,26 @@ import tensorflow
 np.set_printoptions(suppress=True)
 
 
+# Create an instance of TKinter Window or frame
 model = tensorflow.keras.models.load_model('keras_model.h5')
 
 cap=cv2.VideoCapture(0, cv2.CAP_DSHOW)
 face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-while cap.isOpened():
-    ref,frame=cap.read()
+win = Tk()
+# Set the size of the window
+win.geometry("1200x700")
+win.title("Camera Anti Virus")
+# Create a Label to capture the Video frames
+label =Label(win)
+
+label1 = Label(win,text = "MASK:",font=("none 12 bold",20) ,bg="silver",width="200", height="3")
+label2 = Label(win,text = "NO MASK:",font=("none 12 bold",20) ,bg="silver",width="200", height="3")
+label.grid(row=0, column=0)
+cap= cv2.VideoCapture(0)
+
+# Define function to show frame
+def show_frames():
+    ref, frame = cap.read()
     face=face_cascade.detectMultiScale(frame,1.1, 4)
     for(x,y,w,h) in face:
         face_img = frame[y:y+h, x:x+w]
@@ -42,11 +62,19 @@ while cap.isOpened():
             cv2.putText(frame,'MASK',((x+w)//2,y+h+20),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),3)
         datet=str(datetime.datetime.now())
         cv2.putText(frame,datet,(400,450),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
-          
-    cv2.imshow('Detect',frame)
-    
-    if cv2.waitKey(1)==ord('q'):
-        break
-    
-cap.release()
-cv2.destroyAllWindows()
+    frame = cv2.flip(frame, 1)
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    img = Image.fromarray(cv2image)
+    imgtk = ImageTk.PhotoImage(image=img)
+    label.place(relx=0.5, rely=0.0, anchor=N)
+    label.imgtk = imgtk
+    label.configure(image=imgtk)
+    label.after(10, show_frames)      
+      
+
+label1.place(relx=0.2, rely=0.8, anchor=N)
+label2.place(relx=0.211, rely=0.9, anchor=N)
+   
+show_frames() 
+
+win.mainloop()
